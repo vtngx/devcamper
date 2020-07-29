685604e1f172d0c0ae82a4ec1d7c421074c46f32
Viet Nguyen <vieth.nguyen2505@gmail.com>
2020-07-29 23:16:43 +0700

    Admin CRUD

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,50 @@ const ErrorResponse = require('./../utils/errorResponse');
 const asyncHandler = require('./../middleware/async');
 const User = require('./../models/User.js');
 
-//Get all bootcamps - GET /api/v1/bootcamps - Public
+//Get all users - GET /api/v1/auth/users - Private/Admin
 exports.getUsers = asyncHandler(async function (req, res, next) {
     res.status(200).json(res.advancedResults);
+});
+
+//Get single user - GET /api/v1/auth/users/:id - Private/Admin
+exports.getUser = asyncHandler(async function (req, res, next) {
+    const user = await User.findById(req.params.id);
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
+//Create user - POST /api/v1/auth/users - Private/Admin
+exports.createUser = asyncHandler(async function (req, res, next) {
+    const user = await User.create(req.body);
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
+//Update user - PUT /api/v1/auth/users/:id - Private/Admin
+exports.updateUser = asyncHandler(async function (req, res, next) {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
+//Delete user - DELETE /api/v1/auth/users/:id - Private/Admin
+exports.deleteUser = asyncHandler(async function (req, res, next) {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,27 @@
 const express = require('express');
-const {getUsers} = require('./../controllers/users');
-
-const advancesResults = require('./../middleware/advancedResults');
+const {
+    getUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('./../controllers/users');
 const User = require('./../models/User');
+const advancesResults = require('./../middleware/advancedResults');
+const {protect, authorize} = require('./../middleware/auth');
 
 const router = express.Router();
 
+router.use(protect);
+router.use(authorize('admin'));
+
 router.route('/')
-    .get(advancesResults(User), getUsers);
+    .get(advancesResults(User), getUsers)
+    .post(createUser);
+
+router.route('/:id')
+    .get(getUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
